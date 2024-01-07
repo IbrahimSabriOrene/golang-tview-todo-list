b815d98d5e322bb440154b5f77ad27cc8a47be4b
IbrahimSabriOrene <ibrahim.oren@hotmail.com>
2024-01-07 03:59:08 +0300

    Added input field, fixed flex and grid sys

--- app/main.go
+++ app/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"os"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
-
-
 
 // Add navbar ->  <F1> menu, <F2> todo list(if exists), <F3> add todo, <F4> remove todo, <F5> update todo
 
@@ -16,11 +16,12 @@ var app = tview.NewApplication()
 var pages = tview.NewPages()
 var navbar = tview.NewList()
 var list = tview.NewList()
-var flex = tview.NewFlex()
-var box = tview.NewBox()
+var inputField = tview.NewInputField()
 var text = tview.NewTextArea()
 var content = tview.NewTextView()
-
+var keyinfo1 = tview.NewTextView()
+var keyinfo2 = tview.NewTextView()
+var keyinfo3 = tview.NewTextView()
 
 func addTodo() {
 
@@ -28,35 +29,35 @@ func addTodo() {
 	pages.SwitchToPage("AddTodo")
 }
 
-func showTodo(){
+func showTodo() {
 
 	pages.AddPage("ShowTodo", content, true, true)
 	pages.SwitchToPage("ShowTodo")
-	
+
 }
 
 func navbarTui() *tview.Pages {
 
 	pages.AddPage("List", list, true, true)
-	list.Clear()
-	list.SetTitle("Menu")
+	//	list.Clear()
+	//	list.SetTitle("Menu")
+	//
+	//	list.SetBorder(true)
+	//	list.AddItem("Add Todo's'", "", 'a', func() {
+	//		addTodo()
+	//	})
+	//
+	//	list.AddItem("quit", "", 'q', func() {
+	//		app.Stop()
+	//	})
+	inputField.SetLabel("Task")
 
-	list.SetBorder(true)
-	list.AddItem("Add Todo's'", "", 'a', func() {
-		addTodo()
-	})
-	
-	list.AddItem("quit", "", 'q', func() {
-		app.Stop()
-	})
 	pages.SwitchToPage("List")
 	return pages
 }
 func saveTodo(app *tview.Application) {
 	filename := "Todo.txt"
 	storedText := text.GetText()
-   // Add the line number and a space before the line
-        fmt.Fprintf(tempFile, "%d %s\n", lineNumber, line)
 	file, err := os.Create(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error while creating the file")
@@ -64,18 +65,21 @@ func saveTodo(app *tview.Application) {
 	}
 	defer file.Close()
 
-	_, err = file.WriteString(storedText)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error while writing file")
 		return
 	}
+
+	_, err = file.WriteString(storedText)
+	// Add the line number and a space before the line
+	setLineNumber() // Move this to somewhere else it keeps repeating
 	text.SetText(storedText, true)
 	content.SetText(storedText)
 
 }
 func loadTodo(app *tview.Application, textarea *tview.TextView) {
 	//on first start this should've start
-	filename:="Todo.txt"
+	filename := "Todo.txt"
 	// Open the file
 	file, err := os.Open(filename)
 	if err != nil {
@@ -91,19 +95,71 @@ func loadTodo(app *tview.Application, textarea *tview.TextView) {
 	}
 
 	textarea.SetText(string(data))
-	text.SetText(string(data),true)
+	text.SetText(string(data), true)
+}
+
+func setLineNumber() {
+	filename := "Todo.txt" // Replace with your filename
+
+	// Open the file in read mode
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a new temporary file to store the modified content
+	tempFile, err := os.Create("temp_file.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating temporary file: %v\n", err)
+		return
+	}
+	defer tempFile.Close()
+
+	// Iterate through each line of the original file
+	scanner := bufio.NewScanner(file)
+	lineNumber := 1
+	for scanner.Scan() {
+		// Get the current line
+		line := scanner.Text()
+
+		// Add the line number and a space before the line
+		fmt.Fprintf(tempFile, "%d %s\n", lineNumber, line)
+
+		lineNumber++
+	}
+
+	// Handle any errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error scanning file: %v\n", err)
+		return
+	}
+
+	// Close both files
+	file.Close()
+	tempFile.Close()
+
+	// Overwrite the original file with the modified content
+	if err := os.Rename("temp_file.txt", filename); err != nil {
+		fmt.Fprintf(os.Stderr, "Error renaming temporary file: %v\n", err)
+		return
+	}
+
+	fmt.Println("Numbers added successfully to each line of the file!")
 }
 
 func handleKeyPress(event *tcell.EventKey) *tcell.EventKey {
 	switch event.Key() {
 	case tcell.KeyF1:
 		pages.SwitchToPage("List")
-		app.SetFocus(list)
-	case tcell.KeyF2:
+		app.SetFocus(text)
+	case tcell.KeyF3:
 		app.SetFocus(content)
 	case tcell.KeyCtrlS:
 		saveTodo(app)
-		
+	case tcell.KeyF2:
+		app.Stop()
 	}
 	return event
 }
@@ -117,18 +173,36 @@ func main() {
 	// (2) DO ANOTHERTHING 313211
 	// (3) DO GOOD THING
 	// (4) GREAT stuff
-	// On pressing enter, remove the current textarea's text, add it to the textview. Example <Enter Pressed> (number) Keep learning golang
+	// On pressing enter, remove the current textarea's text, add it to the textview with (number)
 	// To remove anything, we are gonna use Id = 1 etc. but instead ID = 1, we need better way to remove it, prefer line removal
 	// update by query???
 	// Later we are gonna connect MySql Database
 
 	// The project's cookie resets every 24 hour - changeable
 	//
-	loadTodo(app,content)
-	menu := navbarTui()
-	flex.AddItem(menu, 0, 1, false)
+	keyinfo1.SetText("<F1> Add Todo").SetTextAlign(tview.AlignCenter)
+	keyinfo2.SetText("<Q> Quit").SetTextAlign(tview.AlignCenter)
+	keyinfo3.SetText("<F3> Quit and Save").SetTextAlign(tview.AlignCenter)
+
+	//menu := navbarTui()
+	flex := tview.NewFlex()
+
+	grid := tview.NewGrid().
+		SetRows(3, 0, 3).
+		SetColumns(30, 0, 30).
+		SetBorders(true).
+		AddItem(flex.AddItem(keyinfo1, 0, 2, false).
+			AddItem(keyinfo2, 0, 2, false).
+			AddItem(keyinfo3, 0, 2, false), 2, 0, 1, 3, 1, 1, false).
+		AddItem(inputField, 0, 0, 1, 3, 0, 0, false).
+		AddItem(content, 1, 0, 1, 3, 0, 0, false)
+
+	content.SetTextAlign(tview.AlignCenter)
+
+	loadTodo(app, content)
+	//flex.AddItem(menu, 0, 1, false)
 	//flex.AddItem(tview.NewTextView(), 0,1, false)
-	flex.AddItem( content,0, 3, false)
+	//flex.AddItem(content, 0, 3, false)
 	app.SetInputCapture(handleKeyPress)
-	app.SetRoot(flex, true).SetFocus(menu).Run()
+	app.SetRoot(grid, true).SetFocus(inputField).Run()
 }
